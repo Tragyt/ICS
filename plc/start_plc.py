ee144c9032967e37e3b79288e4a1298b9b6bd3d3
Tragyt <obaf00@gmail.com>
2026-03-07 12:27:34 +0100

    typo fix

--- plc/start_plc.py
+++ plc/start_plc.py
@@ -8,12 +8,12 @@ from bs4 import BeautifulSoup
 load_dotenv() 
 s = requests.Session()
 
-print(os.getenv("OPENPLC_ADMIIN"))
+print(os.getenv("OPENPLC_ADMIN"))
 print(os.getenv("OPENPLC_PASSWORD"))
 
 soup = BeautifulSoup(s.get("http://localhost:8080/login").text, "html.parser")
 csrf = soup.find("input", {"name": "csrf_token"})["value"]
-login = {"username": os.getenv("OPENPLC_ADMIIN"), "password": os.getenv("OPENPLC_PASSWORD"), "csrf_token": csrf}
+login = {"username": os.getenv("OPENPLC_ADMIN"), "password": os.getenv("OPENPLC_PASSWORD"), "csrf_token": csrf}
 
 s.post("http://localhost:8080/login", data=login)
 s.get("http://localhost:8080/compile-program?file=arm_plc.st")
